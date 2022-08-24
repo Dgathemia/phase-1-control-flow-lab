9d9fa5716d8e218e8e961a8b1c05510403fe3922
danielgathemia <danielgathemia01@gmail.com>
2022-08-24 14:19:04 +0300

    complete

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
   // Write your code here!
+
+  function scuberGreetingForFeet(ride){
+    if (ride <= 199) {
+      return 'This one is on me!'
+    } else if (ride <= 2001) {
+      return 'I will gladly take your thirty bucks.'
+    } else if (ride >= 2500) {
+      return 'No can do.'
+    }
+    
+  
+} 
+
+function ternaryCheckCity(someCity){
+  // Write your code here!
+  
+    return ternaryCheckCity = someCity === "NYC"? 'Ok, sounds good.' : 'No go.'
+   
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  
+  switch(tip) {
+    case 'generous':
+    return 'Thank you so much.';
+    break;
+    case 'not as generous':
+      return 'Thank you.';
+    break;
+    default:
+      return 'Bye.';
+    break;
+    }
+    
+  
 }
